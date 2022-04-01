128efcf6aebdc699992115cd6e04b0f1aecb91eb
the.cold.moon <lyliu013@gmail.com>
2022-04-01 19:55:50 -0400

    Add corrections

--- avg_neighbour_value.m
+++ avg_neighbour_value.m
@@ -1,6 +1,6 @@
 function average_neighbour_value = avg_neighbour_value(m,n,grid)
     
-    % Take in only available neighbours. This is 8 niehgbours, so rid of first 4 if doing 4
+    % Take in only available neighbours. This is 8 neighbours, so rid of first 4 if doing 4
     % neighbours
     elements = [];
 
@@ -29,5 +29,5 @@ function average_neighbour_value = avg_neighbour_value(m,n,grid)
         elements = [elements, grid(m, n+1)];
     end
     
-    average_neighbour_value = size(sum(elements(elements~=grid(m,n) & elements~=0),2),'all')/size(elements(elements~=0),2);
+    average_neighbour_value = sum(elements(elements~=grid(m,n) & elements~=0),'all')/size(elements(elements~=0),2);
 end
